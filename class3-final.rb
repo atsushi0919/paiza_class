e8126fff01da31f40e1c096c273d9a026432e887
atsushi0919 <hatake0919@yahoo.co.jp>
2021-04-23 09:07:46 +0900

    update/class3-final

--- class3-final.rb
+++ class3-final.rb
@@ -2,25 +2,50 @@
 # https://paiza.jp/works/mondai/class_primer/class_primer__robot_move
 
 class Robot
-  VX = ["N": 0, "S": 0, "E": 1, "W": -1]
-  VY = ["N": -1, "S": 1, "E": 0, "W": 0]
+  VX = { N: 0, S: 0, E: 1, W: -1 }
+  VY = { N: -1, S: 1, E: 0, W: 0 }
   MOBILITY = [1, 2, 5, 10]
 
   def initialize(x, y, lv)
     @x = x
     @y = y
     @lv = lv
+    @mobility = MOBILITY[lv - 1]
+  end
+
+  def info
+    [@x, @y, @lv]
+  end
+
+  def move(direction)
+    @x += VX[direction] * @mobility
+    @y += VY[direction] * @mobility
+  end
+
+  def lvup
+    return if @lv == 4
+    @lv += 1
+    @mobility = MOBILITY[@lv - 1]
   end
 end
 
 class Factory
-  attr_writer :toolbox_list, :robot_list
+  attr_reader :robot_list
 
   def initialize(h, w, toolbox_list, robot_list)
     @h = h
     @w = w
     @toolbox_list = toolbox_list
     @robot_list = robot_list
+  end
+
+  def move_target_robot(robot_idx, direction)
+    robot = @robot_list[robot_idx]
+    robot.move(direction)
+
+    if @toolbox_list.include?(robot.info[0..1])
+      robot.lvup
+    end
   end
 end
 
@@ -38,7 +63,15 @@ def solve(input_data)
   end
 
   factory = Factory.new(h, w, toolbox_list, robot_list)
-  p factory
+  input_data.each do |operation_params|
+    robot_idx, direction = operation_params.split
+    direction = direction.to_sym
+    robot_idx = robot_idx.to_i - 1
+    factory.move_target_robot(robot_idx, direction)
+  end
+  factory.robot_list.map do |robot|
+    robot.info.join(" ")
+  end
 end
 
 TOOLBOX = 10
