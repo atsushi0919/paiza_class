9b26673583730c371c880076bf0ae350dc5e0896
atsushi0919 <hatake0919@yahoo.co.jp>
2021-12-13 15:09:35 +0900

    class_primer_sort_01-03 C# AC

---
+++ class_primer_sort_01-03/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace class_primer_sort_01_03
+{
+    class Program
+    {
+        static void Main()
+        {
+            int N = int.Parse(Console.ReadLine());
+            List<User> users = new List<User>();
+            for (int i = 0; i < N; i++)
+            {
+                string line = Console.ReadLine();
+                string nickname = line.Split(' ')[0];
+                int old = int.Parse(line.Split(' ')[1]);
+                string birth = line.Split(' ')[2];
+                string state = line.Split(' ')[3];
+                users.Add(new User(nickname, old, birth, state));
+            }
+
+            users.Sort((a, b) => a.old - b.old);
+            foreach (User user in users)
+            {
+                Console.WriteLine(user.Info());
+            }
+        }
+
+        public struct User
+        {
+            public string nickname, birth, state;
+            public int old;
+
+            public User(string nickname, int old, string birth, string state)
+            {
+                this.nickname = nickname;
+                this.old = old;
+                this.birth = birth;
+                this.state = state;
+            }
+
+            public string Info()
+            {
+                return string.Format("{0} {1} {2} {3}", nickname, old, birth, state);
+            }
+        }
+    }
+}
