f8b46422cebe642e57bf944653a77cbc567d8be9
atsushi0919 <hatake0919@yahoo.co.jp>
2021-12-20 17:13:19 +0900

    02-01 解答作成 未提出

--- class_primer_02-01_make_class/Program.cs
+++ class_primer_02-01_make_class/Program.cs
@@ -8,59 +8,45 @@ namespace class_primer_02_01_make_class
     {
         static void Main()
         {
-            string[] input = Console.ReadLine().Split(' ');
-            int N = int.Parse(input[0]);
-
+            int N = int.Parse(Console.ReadLine());
             List<Employee> employees = new List<Employee>();
             for (int i = 0; i < N; i++)
             {
-                input = Console.ReadLine().Split(' ');
-                switch (input[0])
+                string[] input = Console.ReadLine().Split(' ');
+                string req = input[0];
+                int num = int.Parse(input[1]);
+
+                switch (req)
                 {
                     case "make":
-                        Console.WriteLine("Make");
-                        employees.Add(new Employee(input[1]));
+                        string name = input[2];
+                        employees.Add(new Employee(name, num));
                         break;
                     case "getnum":
-                        Console.WriteLine(Employee.employees[int.Parse(input[1]) - 1].number);
+                        Console.WriteLine(employees[num - 1].Number);
                         break;
                     case "getname":
-                        Console.WriteLine("GetName");
+                        Console.WriteLine(employees[num - 1].Name);
                         break;
                 }
-                foreach (var e in employees)
-                {
-                    Console.WriteLine(e);
-                }
-                // string name = input[0];
-                // users.Add(new User(nickname, old, birth, state));
             }
-            // for (int i = 0; i < K; i++)
-            // {
-            //     input = Console.ReadLine().Split(' ');
-            //     int idx = int.Parse(input[0]) - 1;
-            //     string newName = input[1];
-            //     User user = users[idx];
-            //     user.nickname = newName;
-            //     users[idx] = user;
-            // }
-
-            // foreach (User user in users)
-            // {
-            //     Console.WriteLine(user.Info());
-            // }
         }
 
         public class Employee
         {
-            public string name;
-            public int number;
-
-            // コンストラクタ
-            public Employee(string name)
+            public string Name
             {
-                this.name = name;
-                number += 1;
+                get;
+            }
+            public int Number
+            {
+                get;
+            }
+
+            public Employee(string name, int number)
+            {
+                this.Name = name;
+                this.Number = number;
             }
         }
     }
