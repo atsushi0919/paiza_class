3dfb5f8f4d5675edcc251f21e6073d343302e61a
atsushi0919 <hatake0919@yahoo.co.jp>
2021-12-22 17:11:45 +0900

    02-04 問題追加

---
+++ class_primer_02-04_inheritance/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace class_primer_02_04_inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var customer = new Customer(25);
+
+            customer.OrderFood();
+        }
+    }
+
+    class Customer
+    {
+        private int age, payment;
+
+        public Customer(int age)
+        {
+            this.age = age;
+            payment = 0;
+        }
+
+        public void OrderFood()
+        {
+            Console.WriteLine("Order Food");
+        }
+    }
+}
