81f5068307ae35a54897d0277013d436872f9213
atsushi0919 <hatake0919@yahoo.co.jp>
2022-01-06 09:20:22 +0900

    03-04_super_super_supercar 作成

---
+++ class_primer_03-04_super_super_supercar/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace class_primer_03_04_super_super_supercar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
